8ebf08b1663be249eee240ea84bfd7a8673af58e
AlokPrasanna <alokprasanna1104@gmail.com>
2025-04-01 01:38:11 +0530

    Modified constat images

--- src/constants/Images.jsx
+++ src/constants/Images.jsx
@@ -8,11 +8,11 @@ const Images = {
   sample1: "/Images/sample1.jpg",
   sample2: "/Images/sample2.jpg",
   sample3: "/Images/sample3.jpg",
-  sample4: "/Images/sample3.jpg",
+  sample4: "/Images/sample4.jpg",
   sample5: "/Images/sample1.jpg",
   sample6: "/Images/sample2.jpg",
   sample7: "/Images/sample3.jpg",
-  sample8: "/Images/sample3.jpg",
+  sample8: "/Images/sample4.jpg",
 };
 
 // -------------------- Exports --------------------
